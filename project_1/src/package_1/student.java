61c1aff922d54467e978847b98037b89274449d1
USER <USER@DESKTOP-P2HTOSB>
2023-09-08 22:14:02 +0600

    pushing the code first time

---
+++ project_1/src/package_1/student.java
@@ -0,0 +1,25 @@
+package package_1;
+
+public class student 
+{
+ int age;
+ int rollno;
+
+
+public static void main(String[] args) 
+  {
+  student ob=new student();
+  ob.age=27;
+  ob.rollno=16;
+  System.out.println("value of age "+ob.age);
+  System.out.println("value of rollno "+ob.rollno);
+  }
+public void display1() 
+{
+System.out.println("welcome to all of you");
+}
+public void display2()
+{
+System.out.println("automation is very easy");
+}
+}
